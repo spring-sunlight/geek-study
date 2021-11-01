b470f4b339f805b4dc2a36d02b57bc02c9c6e52d
xunchunyang <xunchunyang@qianxin.com>
2021-11-01 10:18:20 +0800

    add forth week job

---
+++ algorithm/forth-week/convertBST.go
@@ -0,0 +1,22 @@
+package forth_week
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func convertBST(root *TreeNode) *TreeNode {
+	sum := 0
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node != nil {
+			dfs(node.Right)
+			sum += node.Val
+			node.Val = sum
+			dfs(node.Left)
+		}
+	}
+	dfs(root)
+	return root
+}
