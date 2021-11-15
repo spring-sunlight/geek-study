e7a8f454d538570478170c796f3c537dff4fc077
xunchunyang <xunchunyang@qianxin.com>
2021-11-15 16:01:00 +0800

    add sixth week work

--- algorithm/sixth-week/climbStairs.go
+++ algorithm/sixth-week/climbStairs.go
@@ -1,1 +1,11 @@
 package sixth_week
+
+func climbStairs(n int) int {
+	p, q, r := 0, 0, 1
+	for i := 1; i <= n; i++ {
+		p = q
+		q = r
+		r = p + q
+	}
+	return r
+}
--- algorithm/sixth-week/findNumberOfLIS.go
+++ algorithm/sixth-week/findNumberOfLIS.go
@@ -1,1 +1,31 @@
 package sixth_week
+
+//参考了题解
+
+func findNumberOfLIS(nums []int) (ans int) {
+	maxLen := 0
+	n := len(nums)
+	dp := make([]int, n)
+	cnt := make([]int, n)
+	for i, x := range nums {
+		dp[i] = 1
+		cnt[i] = 1
+		for j, y := range nums[:i] {
+			if x > y {
+				if dp[j]+1 > dp[i] {
+					dp[i] = dp[j] + 1
+					cnt[i] = cnt[j] // 重置计数
+				} else if dp[j]+1 == dp[i] {
+					cnt[i] += cnt[j]
+				}
+			}
+		}
+		if dp[i] > maxLen {
+			maxLen = dp[i]
+			ans = cnt[i] // 重置计数
+		} else if dp[i] == maxLen {
+			ans += cnt[i]
+		}
+	}
+	return
+}
